b607f88ec858789e7cd24c35e6286f9841c7285f
shannon <sean@acrite.ly>
2016-12-26 19:54:57 -0800

    Tested on Win7 & Linux
    tested on vm’s

--- HostsFileSync.py
+++ HostsFileSync.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # Hosts File Sync script
 # Sean Hannon 2016
@@ -9,26 +10,35 @@
 import os
 import sys
 import urllib2
-#import getpass
 
 #build path to existing hosts file
 local_platform = sys.platform
+host_path = ''
+
+print "reading OS..."
 
 #switch on os type
 if sys.platform.startswith('darwin') :
    host_path = '/private/etc/hosts'
+   
+if sys.platform.startswith('win') :
+   host_path = 'C:\Windows\System32\drivers\etc\hosts'
 
-#if sys.platform.startswith('linux') :
-# todo windows
-
-#user = getpass.getuser()
-
+if sys.platform.startswith('linux') :
+	host_path = '/etc/hosts'
+	
+if host_path is '' :
+	print "Unable to resolve OS"
+	print sys.platform
+	sys.exit()
+else :
+	print 'updating: ' + host_path
 
 url = 'http://someonewhocares.org/hosts/zero/hosts'
 open_url = urllib2.urlopen(url)
 
 #write file to hosts path
 with open(host_path, 'w') as f:
-  #  f.write(local_platform)	
-   # f.write(user)
     f.write(open_url.read())
+
+print 'host file updated!'	
